1e84d1e3fe4a9097988c0234e77f0f3fb978b0b2
lvqingan <lvqingan@gmail.com>
2017-01-04 11:08:30 +0800

    update

--- backupmongodb.sh
+++ backupmongodb.sh
@@ -27,28 +27,23 @@ for KEY in "${!DBHOST[@]}"; do
   echo "Backing up MongoDB database on ${DBHOST[$KEY]}..."
 
   DBS=${DBNAMES[$KEY]}
+  COLLECTIONS=''
 
   # filter out the collections to backup
-  if [ -n "${DBCOLLECTIONS[$KEY]}" ]; then
-    if  [ ${DBCOLLECTIONSMATCH[$KEY]} = "exclude" ]; then
-      COLLECTIONS=''
-      for collection in ${DBCOLLECTIONS[$KEY]}; do
-        COLLECTIONS="$COLLECTIONS --excludeCollection=$collection "
-      done
-    else
-      COLLECTIONS=''
-      for collection in ${DBCOLLECTIONS[$KEY]}; do
-        COLLECTIONS="$COLLECTIONS --collection=$collection "
-      done
-    fi
+  if [ -n "${DBCOLLECTIONSEXCLUDE[$KEY]}" ]; then
+    for collection in ${DBCOLLECTIONSEXCLUDE[$KEY]}; do
+      COLLECTIONS="$COLLECTIONS --excludeCollection=$collection "
+    done
   fi
 
   for database in $DBS; do
     echo -n "Backing up database $database..."
     test ${DBHOST[$KEY]} = "localhost" && SERVER=`hostname -f` || SERVER=${DBHOST[$KEY]}
-    mongodump --host ${DBHOST[$KEY]} --port ${DBPORT[$KEY]} --username=${DBUSER[$KEY]} --password=${DBPASS[$KEY]} \
-      ${DBOPTIONS[$KEY]} --db $database $COLLECTIONS > $BACKDIR/$SERVER-$database-$DATE-mongodbbackup.sql
-    $COMPRESSION_COMMAND $BACKDIR/$SERVER-$database-$DATE-mongodbbackup.sql
+    mongodump --host=${DBHOST[$KEY]} --port=${DBPORT[$KEY]} --username=${DBUSER[$KEY]} --password=${DBPASS[$KEY]} \
+      ${DBOPTIONS[$KEY]} --db=$database $COLLECTIONS --out=$BACKDIR/$SERVER-$database-$DATE-mongodbbackup
+    cd $BACKDIR
+    $COMPRESSION_COMMAND $SERVER-$database-$DATE-mongodbbackup.$COMPRESSION_EXTENSION $SERVER-$database-$DATE-mongodbbackup
+    rm -rf $SERVER-$database-$DATE-mongodbbackup
     echo "done!"
   done
 done
@@ -59,8 +54,8 @@ done
 
 if  [ $MAIL = "y" ]; then
   BODY="Your backup is ready! Find more useful scripts and info at http://www.ameir.net. \n\n"
-  BODY=$BODY`cd $BACKDIR; for file in *$DATE-mongodbbackup.sql.$COMPRESSION_EXTENSION; do md5sum ${file};  done`
-  ATTACH=`for file in $BACKDIR/*$DATE-mongodbbackup.sql.$COMPRESSION_EXTENSION; do echo -n "-a ${file} ";  done`
+  BODY=$BODY`cd $BACKDIR; for file in *$DATE-mongodbbackup.$COMPRESSION_EXTENSION; do md5sum ${file};  done`
+  ATTACH=`for file in $BACKDIR/*$DATE-mongodbbackup.$COMPRESSION_EXTENSION; do echo -n "-a ${file} ";  done`
 
   echo -e "$BODY" | mutt -s "$SUBJECT" $ATTACH -- $EMAILS
   if [[ $? -ne 0 ]]; then
@@ -71,7 +66,7 @@ if  [ $MAIL = "y" ]; then
 fi
 
 if  [ $DELETE = "y" ]; then
-  OLDDBS=`cd $BACKDIR; find . -name "*-mongodbbackup.sql.$COMPRESSION_EXTENSION" -mtime +$DAYS`
+  OLDDBS=`cd $BACKDIR; find . -name "*-mongodbbackup.$COMPRESSION_EXTENSION" -mtime +$DAYS`
   REMOVE=`for file in $OLDDBS; do echo -n -e "delete ${file}\n"; done` # will be used in FTP
 
   cd $BACKDIR; for file in $OLDDBS; do rm -v ${file}; done
@@ -94,7 +89,7 @@ if  [ $FTP = "y" ]; then
   echo "Initiating FTP connection..."
 
   cd $BACKDIR
-  ATTACH=`for file in *$DATE-mongodbbackup.sql.$COMPRESSION_EXTENSION; do echo -n -e "put ${file}\n"; done`
+  ATTACH=`for file in *$DATE-mongodbbackup.$COMPRESSION_EXTENSION; do echo -n -e "put ${file}\n"; done`
 
   for KEY in "${!FTPHOST[@]}"; do
     echo -e "\nConnecting to ${FTPHOST[$KEY]} with user ${FTPUSER[$KEY]}..."
